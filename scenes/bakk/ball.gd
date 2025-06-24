98643e105e4a3e2407661a7b3a2ae6c1094a4f4a
AhkunTa <gqk1213@foxmail.com>
2025-06-25 01:37:38 +0800

    add carried state

--- scenes/bakk/ball.gd
+++ scenes/bakk/ball.gd
@@ -6,6 +6,10 @@ var state_factory :=BallStateFactory.new()
 var velocity:= Vector2.ZERO
 var current_state :BallState = null
 var carrier :Player = null
+
+
+
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var player_detection_area :Area2D = $PlayerDetection
 
 
@@ -16,7 +20,7 @@ func switch_state(state: Ball.State) -> void:
 	if current_state != null:
 		current_state.queue_free()
 	current_state = state_factory.get_fresh_state(state)
-	current_state.setup(self, player_detection_area,carrier)
+	current_state.setup(self, player_detection_area,carrier, animation_player)
 	current_state.state_transition_requested.connect(switch_state.bind())
 	current_state.name = "BallStateMachine"
 	call_deferred('add_child', current_state)
--- scenes/ball/ball_states/ball_state_carried.gd
+++ scenes/ball/ball_states/ball_state_carried.gd
@@ -2,8 +2,26 @@ class_name BallStateCarried
 
 extends BallState
 
+var OFFSET_FROM_PLAYER: Vector2 = Vector2(10,-10)
+var DRIBBLE_FREQUENCY: float = 10.0
+var DRIBBLEe_INTENSITY: float = 3.0
+
+var dribble_time := 0.0
 func _enter_tree() -> void:
 	assert(carrier != null)
 
 func _process(delta: float) -> void:
-	ball.position = carrier.position
+	var vx: float = 0.0
+	dribble_time += delta
+	if carrier.velocity != Vector2.ZERO:
+		if carrier.velocity.x != 0:
+			vx = cos(DRIBBLE_FREQUENCY * dribble_time) * DRIBBLEe_INTENSITY
+		if carrier.heading.x >=0:
+			animation_player.play("roll")
+			animation_player.advance(0)
+		else:
+			animation_player.play_backwards('roll')
+			animation_player.advance(0)
+	else:
+		animation_player.play('idle')
+	ball.position = carrier.position + Vector2(vx + carrier.heading.x * OFFSET_FROM_PLAYER.x , carrier.heading.y * OFFSET_FROM_PLAYER.y)
