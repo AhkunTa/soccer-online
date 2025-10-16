02f0db09e41f0caa4d12a4a5d94637fcdb05a7ad
AhkunTa <gqk1213@foxmail.com>
2025-10-16 19:22:57 +0800

    feat: enhance AI movement logic and add steering force calculations

--- scenes/goal/goal.gd
+++ scenes/goal/goal.gd
@@ -1,5 +1,4 @@
-
-class_name  Goal
+class_name Goal
 extends Node2D
 
 @onready var back_net_area := %BackNetArea
@@ -15,3 +14,7 @@ func on_ball_enter_back_net(ball: Ball) -> void:
 
 func get_random_target_position() -> Vector2:
 	return targets.get_child(randi_range(0, targets.get_child_count() - 1)).global_position
+
+
+func get_center_target_position() -> Vector2:
+	return targets.get_child(int(targets.get_child_count() / 2.0)).global_position;
