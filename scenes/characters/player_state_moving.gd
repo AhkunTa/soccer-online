488de218a345f936bc3069e1e33387adfc4e0c3a
AhkunTa <gqk1213@foxmail.com>
2026-02-02 20:33:08 +0800

    feat: combo keys

--- scenes/characters/player_state_moving.gd
+++ scenes/characters/player_state_moving.gd
@@ -20,32 +20,28 @@ func handle_human_movement() -> void:
 	if player.velocity != Vector2.ZERO:
 		teammate_detection_area.rotation = player.velocity.angle()
 
-	# FIXME 缓冲组合按键处理
-	var pass_just_pressed := KeyUtils.is_action_just_pressed(player.control_scheme, KeyUtils.Action.PASS)
-	var shoot_just_pressed := KeyUtils.is_action_just_pressed(player.control_scheme, KeyUtils.Action.SHOOT)
-	var pass_pressed := KeyUtils.is_action_pressed(player.control_scheme, KeyUtils.Action.PASS)
-	var shoot_pressed := KeyUtils.is_action_pressed(player.control_scheme, KeyUtils.Action.SHOOT)
-
-	# 如果刚按下 PASS 且 SHOT 已按下，或刚按下 SHOT 且 PASS 已按下，则触发跳跃
-	if (pass_just_pressed and shoot_pressed) or (shoot_just_pressed and pass_pressed):
+	if KeyUtils.are_actions_pressed_together(player.control_scheme, [KeyUtils.Action.PASS, KeyUtils.Action.SHOOT]):
+		KeyUtils.consume_actions(player.control_scheme, [KeyUtils.Action.PASS, KeyUtils.Action.SHOOT])
 		transition_state(Player.State.JUMPING)
-	elif pass_just_pressed:
+		return
+
+	if KeyUtils.should_trigger_single_action(player.control_scheme, KeyUtils.Action.PASS):
 		if player.has_ball():
 			transition_state(Player.State.PASSING)
 		elif can_teammate_pass_ball():
 			ball.carrier.get_pass_request(player)
 		else:
 			player.swap_requested.emit(player)
-	elif shoot_just_pressed:
+		return
+
+	if KeyUtils.should_trigger_single_action(player.control_scheme, KeyUtils.Action.SHOOT):
 		if player.has_ball():
 			transition_state(Player.State.PREPPING_SHOT)
 		elif ball.can_air_interact():
 			if player.velocity == Vector2.ZERO:
 				if player.is_facing_target_goal():
-					# 球员面向目标门 VOLLEY_KICK
 					transition_state(Player.State.VOLLEY_KICK)
 				else:
-					# 球员背对目标门 BICYCLE_KICK
 					transition_state(Player.State.BICYCLE_KICK)
 			else:
 				transition_state(Player.State.HEADER)
--- utils/key_utils.gd
+++ utils/key_utils.gd
@@ -5,6 +5,11 @@ enum Action {LEFT, RIGHT, UP, DOWN, SHOOT, PASS, JUMP}
 # 缓冲 时间窗口
 const TOLERANCE_WINDOW = 150  # 毫秒
 
+# 定义组合键配置: [动作1, 动作2]
+const COMBO_KEYS: Array[Array] = [
+	[Action.PASS, Action.SHOOT],  # 跳跃组合键
+]
+
 const ACTIONS_MAP: Dictionary = {
 	Player.ControlScheme.P1: {
 		Action.LEFT: "p1_left",
@@ -26,6 +31,17 @@ const ACTIONS_MAP: Dictionary = {
 	},
 }
 
+# TODO 先简单实现 组合键检测 后续看情况 实现 输入容错窗口 
+# 记录待定的单键按下时间戳
+static var _pending_actions: Dictionary = {}
+
+static func _init_dicts() -> void:
+	if _pending_actions.is_empty():
+		for scheme in ACTIONS_MAP.keys():
+			_pending_actions[scheme] = {}
+			for action in Action.values():
+				_pending_actions[scheme][action] = 0
+
 static func check_input() -> void:
 	pass
 
@@ -46,3 +62,46 @@ static func is_action_just_released(scheme: Player.ControlScheme, action: Action
 
 static func is_action_both_pressed(scheme: Player.ControlScheme, action1: Action, action2: Action) -> bool:
 	return Input.is_action_pressed(ACTIONS_MAP[scheme][action1]) and Input.is_action_pressed(ACTIONS_MAP[scheme][action2])
+
+# 检查 PASS + SHOOT 组合键
+static func are_actions_pressed_together(scheme: Player.ControlScheme, actions: Array[Action]) -> bool:
+	_init_dicts()
+
+	if actions.size() != 2:
+		return false
+
+	var time1 = _pending_actions[scheme][actions[0]]
+	var time2 = _pending_actions[scheme][actions[1]]
+
+	if time1 == 0 or time2 == 0:
+		return false
+	return abs(time1 - time2) <= TOLERANCE_WINDOW
+
+static func should_trigger_single_action(scheme: Player.ControlScheme, action: Action) -> bool:
+	_init_dicts()
+	if Input.is_action_just_pressed(ACTIONS_MAP[scheme][action]):
+		# 如果是组合键的一部分，标记为 待定 不立即触发
+		if _is_part_of_combo(action):
+			_pending_actions[scheme][action] = Time.get_ticks_msec()
+			return false
+		else:
+			return true
+
+	# 检查是否有待定的按键
+	var pending_time = _pending_actions[scheme][action]
+	if pending_time > 0:
+		var elapsed = Time.get_ticks_msec() - pending_time
+
+		# 如果等待期已过，触发单键
+		if elapsed > TOLERANCE_WINDOW:
+			_pending_actions[scheme][action] = 0  # 清除待定
+			return true
+
+	return false
+
+# 检查动作是否是组合键的一部分
+static func _is_part_of_combo(action: Action) -> bool:
+	for combo in COMBO_KEYS:
+		if action in combo:
+			return true
+	return false
