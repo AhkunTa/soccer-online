6b9dfecfc48b6d48d8d56269a3ee9e1b8cd6df7f
AhkunTa <gqk1213@foxmail.com>
2026-02-25 15:26:43 +0800

    feat: add tournament screen

---
+++ scenes/screens/tournament/bracket_flag.gd
@@ -0,0 +1,16 @@
+class_name BracketFlag
+extends TextureRect
+
+@onready var border: TextureRect = %Border
+@onready var score_label: Label = %ScoreLabel
+
+
+func set_as_current_team() -> void:
+	border.visible = true
+
+func set_as_winner(score: String) -> void:
+	score_label.text = score
+	score_label.visible = true
+	
+func set_as_loser() -> void:
+	modulate = Color(.2, .2, .2, 1)
